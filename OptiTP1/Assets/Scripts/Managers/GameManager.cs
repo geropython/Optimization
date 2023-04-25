24bd1e0506849b9b507ddfc91a136ae9acd3d8c2
Durzdor <maxiperez024@hotmail.com>
2023-04-25 14:33:51 -0300

    MOVER TOMI FOLDER
    die tomi folder die

---
+++ OptiTP1/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Variable del PLayerTank
+    [SerializeField] private GameObject tankPrefab; // prefab del tanque
+    [SerializeField] private Transform spawnPoint; // punto de spawn del tanque
+
+    // Singleton instance variable
+
+    // Propiedad publica para acceder a la variable.
+    public static GameManager Instance { get; private set; }
+
+    public ProjectileManager ProjectilePool { get; private set; }
+
+    private void Awake()
+    {
+        // Si ya hay otra instancia, destruye ésta.
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        // De lo contrario, propone ésta como instancia.
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        SpawnTank();
+        ProjectilePool = GetComponent<ProjectileManager>();
+    }
+
+    private void Start()
+    {
+        ProjectilePool.ResetPool();
+    }
+
+    private void SpawnTank()
+    {
+        // spawn del tanque en el punto designado desde el inspector
+        Instantiate(tankPrefab, spawnPoint.position, Quaternion.identity);
+    }
+}
