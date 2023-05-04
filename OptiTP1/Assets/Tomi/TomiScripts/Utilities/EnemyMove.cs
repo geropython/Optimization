0a7098f86609911ea6a79f20175f7d666a6b6601
Tomi <63859937+tomicancheff@users.noreply.github.com>
2023-05-04 13:21:04 -0300

    New LVL set up, EnemyMove changes
    New scene (tomi) set up for testing
    Changed EnemyMove Script but with no results :(

--- OptiTP1/Assets/Tomi/TomiScripts/Utilities/EnemyMove.cs
+++ OptiTP1/Assets/Tomi/TomiScripts/Utilities/EnemyMove.cs
@@ -12,33 +12,53 @@ public class EnemyMove : MonoBehaviour
     {
         if (waypoints.Length == 0) return;
 
-        Vector3 direction = waypoints[_currentWaypointIndex].position - transform.position;
-        direction.y = 0;
-        // PARA QUE NO VAYA EN DIAGONAL
-        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.z))
+        Vector3 currentPosition = transform.position;
+        Vector3 newPosition = waypoints[_currentWaypointIndex].position;
+
+// Ajusta la posición para permitir solo movimiento vertical y horizontal
+        if (Mathf.Abs(newPosition.x - currentPosition.x) > Mathf.Abs(newPosition.z - currentPosition.z))
         {
-            direction.z = 0;
+            newPosition.z = currentPosition.z;
         }
         else
         {
-            direction.x = 0;
+            newPosition.x = currentPosition.x;
         }
-        // print(direction);
-        // direction.normalized;
+
+        Vector3 direction = newPosition - currentPosition;
+        direction.y = 0;
         transform.position += direction.normalized * (speed * Time.deltaTime);
-        
-        
+
         if (Vector3.Distance(transform.position, waypoints[_currentWaypointIndex].position) < 0.1f)
         {
-            _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
+            // Obtener un índice de waypoint aleatorio
+            int newIndex = Random.Range(0, waypoints.Length);
+
+            // Asegurarse de que no se seleccione el mismo waypoint actual
+            while (newIndex == _currentWaypointIndex)
+            {
+                newIndex = Random.Range(0, waypoints.Length);
+            }
+
+            _currentWaypointIndex = newIndex;
         }
     }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Obstacles"))
         {
-            _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
+            // Obtener un índice de waypoint aleatorio
+            int newIndex = Random.Range(0, waypoints.Length);
+
+            // Asegurarse de que no se seleccione el mismo waypoint actual
+            while (newIndex == _currentWaypointIndex)
+            {
+                newIndex = Random.Range(0, waypoints.Length);
+            }
+
+            _currentWaypointIndex = newIndex;
         }
     }
-    
 }
+
