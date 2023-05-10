3988775d3fea6eab0bae7eb9471bb1468fb0b710
Maxi Perez <55637107+Durzdor@users.noreply.github.com>
2023-05-10 11:33:10 -0300

    fix splash skip?
    quizas ni idea

--- OptiTP1/Assets/Scripts/UI/SplashSkip.cs
+++ OptiTP1/Assets/Scripts/UI/SplashSkip.cs
@@ -1,11 +1,18 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SplashSkip : ManagedUpdateBehaviourUI
 {
+    private int _currSceneIndex;
+
+    private void Awake()
+    {
+        _currSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
     public override void UpdateMe()
     {
-        var scene = SceneManager.GetActiveScene().buildIndex;
-        if (Input.GetKeyDown(KeyCode.Space)) SceneManager.LoadScene(scene + 1);
+        if (Input.GetKey(KeyCode.Space)) SceneManager.LoadScene(_currSceneIndex + 1);
     }
 }
